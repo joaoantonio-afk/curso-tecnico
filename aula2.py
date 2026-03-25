40974313072d1f22581d8511804dc1fc5853f593
joaoantonio-afk <antoniojoao8865@gmail.com>
2026-03-25 12:44:12 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,13 @@
+nota1 = int(input("Digite a nota 1:"))
+nota2 = int(input("Digite a nota 2:"))
+nota3 = int(input("Digite a nota 3:"))
+nota4 = int(input("Digite a nota 4:"))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+print("A média do aluno é ", media)
+
+if media >= 6:
+    print("Aluno Aprovado")
+else:
+    print("Aluno Reprovado")
---
+++ maiorque10.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite o numero"))
+
+if numero>10:
+    print("Número maior que 10")
+else: 
+    print("Número menor que 10")
